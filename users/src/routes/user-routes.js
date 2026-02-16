2254b739e3489651b05fb4dabc0bb9ad6aec93a9
AdrianGutierrezGit <adriangutierrez2005@gmail.com>
2026-02-16 13:52:30 +0100

    add the router that was not defined

--- users/src/routes/user-routes.js
+++ users/src/routes/user-routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { createUser } from '../service/user-service.js';
+const router = express.Router();
 
 router.post('/createuser', async (req, res) => {
   try {
