3bb32994f34f2c0a3b6462aaad5f58a4f40034ba
AdrianGutierrezGit <adriangutierrez2005@gmail.com>
2026-02-22 15:00:45 +0100

    Implementation of the bot with a strategy that prioritizes blocking

--- gamey/src/cli.rs
+++ gamey/src/cli.rs
@@ -7,7 +7,7 @@
 //! - Server: Run as an HTTP server for bot API
 
 use crate::{
-    Coordinates, GameAction, GroupExpansionBot, Movement, RandomBot, RenderOptions, YBot, YBotRegistry, game
+    Coordinates, GameAction, GroupExpansionBot,PriorityBlockBot, Movement, RandomBot, RenderOptions, YBot, YBotRegistry, game
 };
 use crate::{GameStatus, GameY, PlayerId};
 use anyhow::Result;
@@ -69,7 +69,9 @@ pub fn run_cli_game() -> Result<()> {
     let args = CliArgs::parse();
     let mut render_options = crate::RenderOptions::default();
     let mut rl = DefaultEditor::new()?;
-    let bots_registry = YBotRegistry::new().with_bot(Arc::new(RandomBot)).with_bot(Arc::new(GroupExpansionBot));
+    let bots_registry = YBotRegistry::new().with_bot(Arc::new(RandomBot))
+    .with_bot(Arc::new(GroupExpansionBot))
+    .with_bot(Arc::new(PriorityBlockBot));
     let bot: Arc<dyn YBot> = match bots_registry.find(&args.bot) {
         Some(b) => b,
         None => {
