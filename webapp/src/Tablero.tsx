75c7545b372dfedbd21cfb7fdeb7905203f3a17c
SergioArgu <uo299741@uniovi.es>
2026-02-24 19:30:08 +0100

    Añado css para el tablero

--- webapp/src/Tablero.tsx
+++ webapp/src/Tablero.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Tablero.css";
 
 const TAM_TABLERO = 10;
 
@@ -7,31 +8,24 @@ const GameBoard: React.FC = () => {
     const filas = [];
 
     for (let i = 0; i < TAM_TABLERO; i++) {
-      const casillas = [];
-      for (let j = 0; j <= i; j++) {
-        casillas.push(
-          <div key={`${i} ${j}`} className="casilla"></div>
+        const casillas = [];
+        for (let j = 0; j <= i; j++) {
+            casillas.push( <div key={`${i} ${j}`} className="casilla"></div>);
+        }
+        
+        filas.push( <div key={i} className="tablero">{casillas}</div>//Casillas de cada fila
         );
-      }
-
-      filas.push(
-        <div key={i} className="tablero">
-          {casillas}
-        </div>//Casillas de cada fila
-      );
     }
 
     return filas;
-  };
+};
 
-  return (
+return (
     <div className="gameBoard">
-      <h2>Juego Y</h2>
-
-      <div className="board">
-        {generarTablero()}
-      </div>
-    
+        <h2>Juego Y</h2>
+        <div className="board">
+            {generarTablero()}
+        </div>
     </div>
   );
 };
