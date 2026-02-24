0c936badeab0ea3bc7ece34aaa4077bd806cd91c
SergioArgu <uo299741@uniovi.es>
2026-02-24 22:55:49 +0100

    Añado opcion de idioma al menu y cambios en css

--- webapp/src/MenuOpciones.tsx
+++ webapp/src/MenuOpciones.tsx
@@ -29,6 +29,7 @@ const Menu: React.FC = () => {
         </div>
 
         <div className="nav-right">
+          <button>Idioma</button>
           <button className="nav-item perfil">
             <img src={avatar} className="avatar" alt="avatar" />
             <span className="username">{user?.username || "Invitado"}</span>
