4ec51e54f7c37a41a7f90d4bdcd7f21fda639286
SergioArgu <uo299741@uniovi.es>
2026-02-25 12:50:42 +0100

    Añado explicaciones y reglas en el inicio

--- webapp/src/Estadisticas.tsx
+++ webapp/src/Estadisticas.tsx
@@ -1,12 +1,14 @@
 import React, { useState,useEffect  } from "react";
 import avatar from './img/avatar.png';
 import { useNavigate } from 'react-router-dom';
+import "./Menu.css";
 
 
 const Estadisticas: React.FC = () => {
     const [activeTab, setActiveTab] = useState("stats");
     const [user, setUser] = useState<{ userId: string; username: string } | null>(null);
     const navigate = useNavigate();
+
     
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
--- webapp/src/MenuOpciones.tsx
+++ webapp/src/MenuOpciones.tsx
@@ -50,9 +50,34 @@ const Menu: React.FC = () => {
       </nav>
 
       <div className="content">
-        {activeTab === "Inicio" && <p>Panel de inicio</p>}
-        {activeTab === "play" && <p>Panel de juego</p>}
-        {activeTab === "stats" && <p>Panel de estadísticas</p>}
+        <h2>Bienvenido a Game Y</h2>
+        <h3>Introducción</h3>
+        <p>El juego Y es un juego de tablero donde prima la estrategia. El objetivo será conectar los tres lados del teclado antes que tu oponente</p>
+        <h3>Reglas del juego</h3>
+        <ul>
+          <li>Se juega por turnos</li>
+          <li>el ganador será el primero en conectar los tres lados del tablero</li>
+        </ul>
+        <h3>Posibilidades</h3>
+          <ul>
+            <li>Jugar contra un bot</li>
+            <li>Elegir estrategia y nivel de dificultad</li>
+            <li>Consultar estadísticas</li>
+          </ul>
+
+        <h3>Estrategias disponibles</h3>
+          <ul>
+            <li>Estrategia de expansión: el bot intentará, si es posible, marcar casillas vecinas.</li>
+            <li>Monte carlo: el bot calcula las posibles posibilidades y marca la casilla donde más posibilidades tiene de ganar.</li>
+            <li>Prioridad en el bloqueo: El bot marca la casilla más proximida a sus propias piezas y bloquea a las del enemigo, dando prioridad a los bordes.</li>
+            <li>Random: el bot marca una casilla al azar de las disponibles.</li>
+            <li>Shortest path: el bot elige la casillas que maximiza la conexión propia entre los lados del tablero y bloquea las oportunidades del oponente.</li>
+            <li>Bloque simple: el bot elige la casilla vacía más cercana a las piezas del oponente para bloquear su expansión. Si no hay piezas enemigas cercanas, elige aleatoriamente.</li>
+            <li>Triángulo de ataque: El bot busca casillas vacías que conecten varias de sus propias piezas. Si no hay, se expande.</li>
+          </ul>
+
+
+
       </div>
 
     </div>
