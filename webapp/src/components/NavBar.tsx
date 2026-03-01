c97fad729cd9c6e1f1e57a68bc3d18ffcc3d7b05
SergioArgu <uo299741@uniovi.es>
2026-03-01 13:50:22 +0100

    Arreglo navegación a ventana jugar

--- webapp/src/components/NavBar.tsx
+++ webapp/src/components/NavBar.tsx
@@ -26,7 +26,7 @@ const NavBar: React.FC<NavBarProps> = ({ activeTab }) => {
     <nav className="nav-bar">
       <div className="nav-left">
         <button 
-          onClick={() => navigate("/configuracion")}
+          onClick={() => navigate("/jugar")}
           className={`nav-item ${activeTab === "play" ? "active" : ""}`}
         >
           Jugar
