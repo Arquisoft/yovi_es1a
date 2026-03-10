cf4caa875f673856c1e05bd8c350dc3b0dc9a440
RyuVegas <asunitasama@gmail.com>
2026-03-10 11:57:31 +0100

    Añadidos cambios al .css y .tsx para que no se coma el menu de navegación

--- webapp/src/pages/Game.tsx
+++ webapp/src/pages/Game.tsx
@@ -8,29 +8,30 @@ const Game: React.FC = () => {
   const { t } = useLanguage();
   const [isSurrendered, setIsSurrendered] = useState(false);
 
-  const handleSurrenderClick = () => { // Función para manejar el clic en el botón de rendirse
-    if (window.confirm(t("confirmSurrender"))) {// Confirmar rendición
-      setIsSurrendered(true); // Activar el estado de rendición, que se pasará al tablero para finalizar la partida
+  const handleSurrenderClick = () => {
+    if (window.confirm(t("confirmSurrender"))) {
+      setIsSurrendered(true);
     }
-};
+  };
 
   return (
     <div className="game-page">
       <NavBar activeTab="play" />
       <div className="game-container"> 
-        <div className="game-header" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '20px' }}>
-          <h2 className="game-title" style={{ margin: 0 }}>{t("partCurso")}</h2>
+        
+        {/* Cabecera más visual*/}
+        <div className="game-header">
+          <h2 className="game-title">{t("partCurso")}</h2>
           
           <button 
             className="btn-surrender" 
             onClick={handleSurrenderClick}
             disabled={isSurrendered}
           >
-            🏳️ {t("rendirse") || "Rendirse"}
+            {t("rendirse") || "Rendirse"}
           </button>
         </div>
 
-        {/* Le pasamos el estado al tablero */}
         <Tablero surrenderTrigger={isSurrendered} /> 
       </div>
     </div>
